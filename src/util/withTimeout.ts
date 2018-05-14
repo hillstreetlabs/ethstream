7bd43b1a59fe1586f29700e9438dc9e94b55c292
Graham Kaemmer <graham.kaemmer@gmail.com>
2018-05-14 15:56:12 -0700

    [WIP] Start work moving things to typescript without mobx

--- src/util/withTimeout.ts
+++ src/util/withTimeout.ts
@@ -1,17 +1,17 @@
-export default (promise, timeout) => {
+export default <T>(promise: Promise<T>, timeout: number): Promise<T> => {
   return new Promise((resolve, reject) => {
     let timer = setTimeout(
       () => reject(`Timeout of ${timeout}ms exceeded`),
       timeout
     );
     promise
-      .then((...args) => {
+      .then((...args: any[]) => {
         if (timer) {
           clearTimeout(timer);
           resolve(...args);
         }
       })
-      .catch((...args) => {
+      .catch((...args: any[]) => {
         if (timer) {
           clearTimeout(timer);
           reject(...args);
@@ -19,4 +19,3 @@ export default (promise, timeout) => {
       });
   });
 };
-
